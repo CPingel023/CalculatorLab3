508970e3755bf896cee6f0a155e148fc4136913d
CPingel023 <CWPingel@student.fullsail.edu>
2022-04-25 20:06:08 -0400

    Created lab tests project.

---
+++ CalculatorLab3Tests/CalculatorLabTests.cpp
@@ -0,0 +1,1 @@
+#include "CalculatorLabTests.h"
---
+++ CalculatorLab3Tests/CalculatorLabTests.h
@@ -0,0 +1,13 @@
+#pragma once
+#include "../CalculatorLab3/CalculatorProcessor.h"
+#include "../CalculatorLab3/App.h"
+#include "../CalculatorLab3/ButtonFactory.h"
+#include "../CalculatorLab3/IBaseCommand.h"
+#include "../CalculatorLab3/CalculatorWindow.h"
+
+
+class CalculatorLabTests
+{
+
+};
+
